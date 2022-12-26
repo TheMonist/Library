30cb6d2e33e999e3890fc92a2bc97b97881a85a3
Roger Rosena <rogerrosena@gmail.com>
2022-12-26 15:47:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//buttons
+let myLibrary = [];
+let addBook = document.querySelector('.addBook');
+
+//object and constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    //do stuff here
+}
+
+function modal() {
+    //working with modals or popup button
+}
+
+//eventlisteners
+addBook.addEventListener('click', function(){
+    document.querySelector();
+})
