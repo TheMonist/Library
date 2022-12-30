99a34a6610a2e444536dbb7ff4ce46e0c1b9c483
Roger Rosena <rogerrosena@gmail.com>
2022-12-29 20:38:31 -0500

    fixed object constructors and added toggle/dropdown

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ let myLibrary = [];
 const addBook = document.querySelector(".addBook");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay")
-const closeModalBtn = document.querySelector(".btn-close");
+const closeModalBtn = document.querySelector(".btnClose");
 const addLibrary = document.querySelector(".addLibrary");
-//const removeBook = document.querySelector();
 
 //Open Modal
 const openModal = function() {
@@ -24,42 +23,98 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    //this.read = read;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-    //Adding Book to Library (Definition)
-    const titleInput = document.querySelector("#title").value;
-    const authorInput = document.querySelector("#author").value;
-    const pagesInput = document.querySelector("#pages").value;
-    //const readInput document.querySelector("#read").checked;
-    
-    const newBook = new Book(titleInput, authorInput, pagesInput);
+    let newBook = new Book(titleInput, authorInput, pagesInput, readInput);
     myLibrary.push(newBook);
+    displayUI();
 }
-
-//Remove from Page
-function removeBookFromLibrary() {
-    titleInput.value == "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    //readInput.checked = false;
-    //remove div like -> formContainer.classList.remove("container")
-}
+ 
 
 //Add to Page
+function displayUI() {
+    const bookContainer = document.querySelector(".container");
 
-//Validation (Maybe Remove)
-function validateForm() {
-    const inputs = document.querySelectorAll("input");
-    for(let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value === "") {
-            alert("Input Value Is Empty");
-            return false;
+    //Removing previously generated div cards
+    const removeDivs = document.querySelectorAll(".cardBody");
+    for (let i = 0; i < removeDivs.length; i++) {
+        removeDivs[i].remove();
+    };
+    
+    let index = 0;
+    //Loop over library array and display cards
+    myLibrary.forEach(myLibrarys => {
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("cardBody");
+        container.appendChild(cardBody);
+
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("removeBtn");
+        removeBtn.textContent = "REMOVE";
+
+        removeBtn.dataset.linkedArray = index;
+        
+        cardBody.appendChild(removeBtn);
+
+        removeBtn.addEventListener("click", removeBookFromLibrary);
+
+        function removeBookFromLibrary(){
+            let retriveBookToRemove = removeBtn.dataset.linkedArray;
+            myLibrary.splice(parseInt(retriveBookToRemove), 1);
+            cardBody.remove()
+            displayUI();
         }
-    }
-    return true;
+
+        const readStatus = document.createElement("button");
+        readStatus.classList.add("readStatus");
+        readStatus.textContent = "Status";
+        readStatus.dataset.linkedArray = index;
+        cardBody.appendChild(readStatus);
+
+        readStatus.addEventListener("click", toggleRead);
+
+        function toggleRead() {
+            let retreiveBookToToggle = readStatus.dataset.linkedArray;
+            Book.prototype = Object.create(Book.prototype);
+            const toggleBook = newBook;
+
+            if (myLibrary[parseInt(retreiveBookToToggle)].Read == "Yes") {
+                toggleBook.Read = "No";
+                myLibrary[parseInt(retreiveBookToToggle)].Read = toggleBook.Read
+            } else if (myLibrary[parseInt(retreiveBookToToggle)].Read == "No") {
+                toggleBook.Read = "Yes"
+                myLibrary[parseInt(retreiveBookToToggle)].Read = toggleBook.Read
+            }
+        }
+
+        for (let key in myLibrarys) {
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            cardBody.appendChild(para);
+        }
+        index++;
+    })
 }
+
+function intakeData() {
+//Adding Book to Library (Definition)
+    let titleInput = document.getElementById("title").value;
+    let authorInput = document.getElementById("author").value;
+    let pagesInput = document.getElementById("pages").value;
+    let readInput = document.getElementById("read").value;
+
+    if ((titleInput === "") || (authorInput === "") || (pagesInput === "") || (readInput === null)) {
+        return;
+    }
+
+    addBookToLibrary(title, author, pages, read);
+
+    document.getElementById("bookForm").requestFullscreen;
+}
+
+
 
 //Eventlisteners
 addBook.addEventListener("click", openModal);
@@ -70,5 +125,5 @@ document.addEventListener("keydown", function (e) {
         modalClose();
     }
 });
-addLibrary.addEventListener("click", addBookToLibrary);
-//removeBook.addEventListener("click", removeBookFromLibrary);
+
+addLibrary.addEventListener("click", intakeData); 
