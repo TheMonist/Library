e0fa135f56a206165a829586ee6db80790f084ba
Roger Rosena <rogerrosena@gmail.com>
2022-12-31 18:55:08 -0500

    change ui day 2

--- script.js
+++ script.js
@@ -1,131 +1,165 @@
-//Buttons
+// Declare empty array for library
 let myLibrary = [];
 
-//Open and Close Modal
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay")
-const closeModalBtn = document.querySelector(".btnClose");
-const addBook = document.querySelector(".addBook");
-
-const openModal = function() {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-};
-
-const closeModal = function() {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
-
-addBook.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-        modalClose();
-    }
-});
-
-//Object and Constructor
+// Class Object
 class Book {
-    constructor(Title, Author, Pages, Read) {
-        this.Title = Title;
-        this.Author = Author;
-        this.Pages = Pages;
-        this.Read = Read;
-    }
+  constructor(Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
+  }
 }
 
-
-function addBookToLibrary() {
-    let newBook = new Book(Title, Author, Pages, Read);
-    myLibrary.push(newBook);
-    displayUI();
-}
- 
-
-//Add to Page
-function displayUI() {
-    const books = document.querySelector(".books");
-
-    //Removing previously generated div cards
-    const removeDivs = document.querySelectorAll(".cardBody");
-    for (let i = 0; i < removeDivs.length; i++) {
-        removeDivs[i].remove();
-    };
-    
-    let index = 0;
-    //Loop over library array and display cards
-    myLibrary.forEach((myLibrarys) => {
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("cardBody");
-        books.appendChild(cardBody);
-
-        const removeBtn = document.createElement("button");
-        removeBtn.classList.add("removeBtn");
-        removeBtn.textContent = "REMOVE";
-
-        removeBtn.dataset.linkedArray = index;
-        cardBody.appendChild(removeBtn);
-
-        removeBtn.addEventListener("click", removeBookFromLibrary);
-
-        function removeBookFromLibrary(){
-            let retriveBookToRemove = removeBtn.dataset.linkedArray;
-            myLibrary.splice(parseInt(retriveBookToRemove), 1);
-            cardBody.remove();
-            displayUI();
-        }
-
-        const readStatus = document.createElement("button");
-        readStatus.classList.add("readStatus");
-        readStatus.textContent = "Status";
-
-        readStatus.dataset.linkedArray = index;
-        cardBody.appendChild(readStatus);
-
-        readStatus.addEventListener("click", toggleRead);
-
-        function toggleRead() {
-            let retreiveBookToToggle = readStatus.dataset.linkedArray;
-            Book.prototype = Object.create(Book.prototype);
-            const toggleBook = new Book();
-
-            if (myLibrary[parseInt(retreiveBookToToggle)].Read == "Yes") {
-                toggleBook.Read = "No";
-                myLibrary[parseInt(retreiveBookToToggle)].Read = toggleBook.Read;
-            } else if (myLibrary[parseInt(retreiveBookToToggle)].Read == "No") {
-                toggleBook.Read = "Yes";
-                myLibrary[parseInt(retreiveBookToToggle)].Read = toggleBook.Read;
-            }
-            displayUI()
-        }
-
-        for (let key in myLibrarys) {
-            const para = document.createElement("p");
-            para.textContent = (`${key}: ${myLibrary[key]}`);
-            cardBody.appendChild(para);
-        }
-        index++;
-    });
+// Function for adding a new book to the array/library
+function addBookToLibrary(Title, Author, Pages, Read) {
+  let book = new Book(Title, Author, Pages, Read);
+  myLibrary.push(book);
+  displayBooksOnPage();
 }
 
-const addLibrary = document.querySelector(".submitBtn");
-addLibrary.addEventListener("click", intakeData);
+// Function to display library array to cards
+function displayBooksOnPage() {
+  const books = document.querySelector(".books");
 
-function intakeData() {
-//Adding Book to Library (Definition)
-    let Title = document.getElementById("title").value;
-    let Author = document.getElementById("author").value;
-    let Pages = document.getElementById("pages").value;
-    let Read = document.getElementById("read").value;
+  // Remove all previously displayed DOM cards before I loop over array again
+  const removeDivs = document.querySelectorAll(".card");
+  for (let i = 0; i < removeDivs.length; i++) {
+    removeDivs[i].remove();
+  }
 
-    //Validation
-    if (Title === "" || Author === "" || Pages === "" || Read === "") {
-        return;
+  // Loop over the library array and display to the cards
+  let index = 0;
+  myLibrary.forEach((myLibrarys) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    books.appendChild(card);
+
+    // Create remove book button and add class attribute for each array card
+    const removeBookButton = document.createElement("button");
+    removeBookButton.classList.add("remove-book-button");
+    removeBookButton.textContent = "Remove From Library";
+
+    // Link the data attribute of the remove button to the array and card
+    removeBookButton.dataset.linkedArray = index;
+    card.appendChild(removeBookButton);
+
+    // Start event listener/remove array item from array and card from parent div via data link
+    removeBookButton.addEventListener("click", removeBookFromLibrary);
+
+    function removeBookFromLibrary() {
+      let retrieveBookToRemove = removeBookButton.dataset.linkedArray;
+      myLibrary.splice(parseInt(retrieveBookToRemove), 1);
+      card.remove();
+      displayBooksOnPage();
     }
 
-    addBookToLibrary(Title, Author, Pages, Read);
-    
-    document.getElementById("bookForm").reset;
+    // Create read status button and add class attribute for each array card
+    const readStatusButton = document.createElement("button");
+    readStatusButton.classList.add("read-status-button");
+    readStatusButton.textContent = "Change Read Status";
+
+    // Link the data attribute of the toggle read button to the array and card
+    readStatusButton.dataset.linkedArray = index;
+    card.appendChild(readStatusButton);
+
+    // Create event listener/toggle logic for array objects prototype for read status change
+    readStatusButton.addEventListener("click", toggleReadStatus);
+
+    function toggleReadStatus() {
+      let retrieveBookToToggle = readStatusButton.dataset.linkedArray;
+      Book.prototype = Object.create(Book.prototype);
+      const toggleBook = new Book();
+
+      // Run check to see what read value is present to toggle from
+      if (myLibrary[parseInt(retrieveBookToToggle)].Read == "Yes") {
+        toggleBook.Read = "No";
+        myLibrary[parseInt(retrieveBookToToggle)].Read = toggleBook.Read;
+      } else if (myLibrary[parseInt(retrieveBookToToggle)].Read == "No") {
+        toggleBook.Read = "Yes";
+        myLibrary[parseInt(retrieveBookToToggle)].Read = toggleBook.Read;
+      }
+      displayBooksOnPage();
+    }
+
+    // Loop over the object keys and values and display to each card
+    for (let key in myLibrarys) {
+      console.log(`${key}: ${myLibrarys[key]}`);
+      const para = document.createElement("p");
+      para.textContent = `${key}: ${myLibrarys[key]}`;
+      card.appendChild(para);
+    }
+
+    index++;
+  });
 }
+
+// Start event listener/display form to add a new book to library
+const addBookButton = document.querySelector(".add-book-button");
+addBookButton.addEventListener("click", displayTheForm);
+
+function displayTheForm() {
+  document.getElementById("add-book-form").style.display = "";
+}
+
+// Start event listener/add input to array for new entry form
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", intakeFormData);
+
+// Transform form data to variables for intake
+function intakeFormData() {
+  let Title = document.getElementById("Title").value;
+  let Author = document.getElementById("Author").value;
+  let Pages = document.getElementById("Pages").value;
+  let Read = document.getElementById("Read").value;
+
+  // Break out if form is incomplete or not valid
+  if (Title == "" || Author == "" || Pages == "" || Read == "") {
+    return;
+  }
+
+  // Call function to input the book data to array
+  addBookToLibrary(Title, Author, Pages, Read);
+
+  // Reset the form after successful submission
+  document.getElementById("add-book").reset();
+}
+
+// Start event listener for clear form button
+const clearButton = document.querySelector(".reset-button");
+clearButton.addEventListener("click", clearForm);
+
+function clearForm() {
+  document.getElementById("add-book").reset();
+}
+
+// Validate the form with the Constraint Validation API
+const constraintValidationAPI = (() => {
+  const valTitle = document.getElementById("Title");
+
+  valTitle.addEventListener("input", () => {
+    valTitle.setCustomValidity("");
+    valTitle.checkValidity();
+  });
+
+  valTitle.addEventListener("invalid", () => {
+    if (valTitle.value === "") {
+      valTitle.setCustomValidity("Title cannot be blank!");
+    }
+  });
+
+  const valAuthor = document.getElementById("Author");
+
+  valAuthor.addEventListener("input", () => {
+    valAuthor.setCustomValidity("");
+    valAuthor.checkValidity();
+  });
+
+  valAuthor.addEventListener("invalid", () => {
+    if (valAuthor.value === "") {
+      valAuthor.setCustomValidity("Author cannot be blank!");
+    }
+  });
+})();
+
+//Refactor using bootstrap
